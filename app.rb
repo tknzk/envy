8d39f1ff5fcfb8a12408c8fdbef0adc136e2c2ce
Purin Kozuke <p@spacepro.be>
2017-07-10 23:54:23 +0900

    define filetype of payload

--- app.rb
+++ app.rb
@@ -18,11 +18,12 @@ class App < Sinatra::Base
 
     content = {}
     content[:inquery_token] = @inquiry_token
-    content[:http_headers] = request.env.select{ |header, value| header.start_with?('HTTP_') }
+    content[:http_headers] = request.env.select{ |header, _| header.start_with?('HTTP_') }
 
     payload = {
       token: ENV['SLACK_TOKEN'],
       channels: ENV['SLACK_CHANNEL'],
+      filetype: 'javascript',
       content: JSON.pretty_generate(content)
     }
 
